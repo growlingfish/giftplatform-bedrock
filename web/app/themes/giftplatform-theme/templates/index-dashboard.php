83946ebf2df2874dabb21fea919eca78ac5e5568
Ben Bedwell <github@growlingfish.com>
2017-07-22 16:16:52 +0100

    Composer

--- web/app/themes/giftplatform-theme/templates/index-dashboard.php
+++ web/app/themes/giftplatform-theme/templates/index-dashboard.php
@@ -7,6 +7,8 @@
  * @license GPL-3.0+
  */
 
+$user = wp_get_current_user();
+
 ?>
 
 <div class="head-logo">
@@ -15,9 +17,9 @@
 
 <div class="step" id="step1">
     <h1>The Recipient</h1>
-    <p>Hello <?php echo wp_get_current_user()->user_firstname; ?>. Who are you making your gift for?</p>
-    <button onclick="step2(false);">Someone I know</button>
-    <button onclick="step2(true);">Someone unknown</button>
+    <p>Hello <?php echo $user->user_firstname; ?>. Who are you making your gift for?</p>
+    <button id="step2_button">Someone I know</button>
+    <button id="step2_strange_button">Someone unknown</button>
 </div>
 
 <div class="step" id="step2a">
@@ -25,14 +27,14 @@
     <p>Who are you making the gift for?</p>
     <p><label for="recipientName">Name:</label><input type="text" name="recipientName" id="recipientName"></p>
     <p><label for="recipientEmail">Email:</label><input type="email" name="recipientEmail" id="recipientEmail"></p>
-    <button onclick="step3a();">Submit</button>
+    <button id="step3_button">Submit</button>
 </div>
 
 <div class="step" id="step2b">
     <h1>Choose a Recipient</h1>
     <p>Who will you make a gift for today?</p>
-    <button onclick="step3b(true);">A local</button>
-    <button onclick="step3b(false);">Someone from out of town</button>
+    <button id="step3_local_button">A local</button>
+    <button id="step3_outoftown_button">Someone from out of town</button>
 </div>
 
 <script>
@@ -41,35 +43,36 @@ var stranger = false;
 jQuery(function($) {
 	$.backstretch('<?php echo get_stylesheet_directory_uri(); ?>/images/backstretch/index-welcome.jpg');
 
-    $('#step2').fadeIn();
-});
+    $('#step1').fadeIn();
 
-function step2 (isStranger) {
-    stranger = isStranger;
-    if (stranger) {
-        jQuery('#step1').slideDown(function () {
-            jQuery('#step2a').slideUp();
+    $('#step2_button').on('click', function () {
+        jQuery('#step1').slideToggle(function () {
+            jQuery('#step2a').slideToggle();
         });
-    } else {
-        jQuery('#step1').slideDown(function () {
-            jQuery('#step2b').slideUp();
+    });
+
+    $('#step2_strange_button').on('click', function () {
+        stranger = true;
+        jQuery('#step1').slideToggle(function () {
+            jQuery('#step2b').slideToggle();
         });
-    }
-}
+    });
 
-function step3a () {
-    if (jQuery('#recipientName').val().length > 0 && jQuery('#recipientEmail').val().length) {
-        console.log(jQuery('#recipientEmail').val());
-    }
-}
+    $('#step3_button').on('click', function () {
+        if (jQuery('#recipientName').val().length > 0 && jQuery('#recipientEmail').val().length) {
+            console.log(jQuery('#recipientEmail').val());
+        }
+    });
 
-function step3b (local) {
-    if (local) {
+    $('#step3_local_button').on('click', function () {
         console.log('localbrighton@gifting.digital');
-        // 31B*CBbd9YS69ElJ3slxSARx
-    } else {
+            // 31B*CBbd9YS69ElJ3slxSARx
+
+    });
+
+    $('#step3_outoftown_button').on('click', function () {
         console.log('outoftownbrighton@gifting.digital');
-        // 9u@2W*hvZpZh!lilxkVDWPZ1
-    }
-}
+            // 9u@2W*hvZpZh!lilxkVDWPZ1
+    });
+});
 </script>
--- web/app/themes/giftplatform-theme/templates/index-welcome.php
+++ web/app/themes/giftplatform-theme/templates/index-welcome.php
@@ -17,14 +17,14 @@
     <h1>Welcome!</h1>
     <p>The GIFT Platform is a set of tools, including this website, that lets you send and receive 'gifts'.</p>
     <p>It is a service provided by the EU-funded GIFT project, which is investigating ways to combine digital content with real cultural artifacts - museum exhibits, historical sites, and so on - to provide new ways for visitors to engage with heritage.</p>
-    <button onclick="step2();">Continue</button>
+    <button id="step2_button">Continue</button>
 </div>
 
 <div class="step" id="step2">
     <h1>What is this website?</h1>
     <p>This website lets you make a gift for another user. The gifts are personal messages that you create for another user to experience when they have found particular exhibits in the museum.</p>
     <p>Using this website you choose the exhibit, create the gift, and send it to the lucky recipient.</p>
-    <button onclick="step3();">OK</button>
+    <button id="step3_button">OK</button>
 </div>
 
 <div class="step" id="step3">
@@ -37,17 +37,17 @@ jQuery(function($) {
 	$.backstretch('<?php echo get_stylesheet_directory_uri(); ?>/images/backstretch/index-welcome.jpg');
 
     $('#step1').fadeIn();
+
+    $('#step2_button').on('click', function () {
+        jQuery('#step1').slideToggle(function () {
+            jQuery('#step2').slideToggle();
+        });
+    });
+
+    $('#step3_button').on('click', function () {
+        jQuery('#step2').slideToggle(function () {
+            jQuery('#step3').slideToggle();
+        });
+    });
 });
-
-function step2 () {
-    jQuery('#step1').slideDown(function () {
-        jQuery('#step2').slideUp();
-    });
-}
-
-function step3 () {
-    jQuery('#step2').slideDown(function () {
-        jQuery('#step3').slideUp();
-    });
-}
 </script>
